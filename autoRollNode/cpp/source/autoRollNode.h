0789798a5fe52da9067927c5153786014220d622
Dilen Shah <dilenshah23@gmail.com>
2023-02-20 17:33:21 +1030

    Update autoRollNode.h

--- autoRollNode/cpp/source/autoRollNode.h
+++ autoRollNode/cpp/source/autoRollNode.h
@@ -1,24 +1,39 @@
+#ifndef AUTOROLLNODE_H
+#define AUTOROLLNODE_H
+
 #include <maya/MPxNode.h>
 #include <maya/MFnNumericAttribute.h>
+#include <maya/MFnMatrixAttribute.h>
 #include <maya/MFnUnitAttribute.h>
-#include <maya/MTypeId.h>
-#include <maya/MPlug.h>
+#include <maya/MFnCompoundAttribute.h>
+#include <maya/MFnPlugin.h>
 #include <maya/MDataBlock.h>
-#include <maya/MEulerRotation.h>
+#include <maya/MDataHandle.h>
+#include <maya/MMatrix.h>
+#include <maya/MVector.h>
+#include <maya/MQuaternion.h>
+#include <maya/MTransformationMatrix.h>
 
-class AutoRoll : public MPxNode
-{
+class AutoRollNode : public MPxNode {
 public:
-    AutoRoll();
-    virtual ~AutoRoll();
-
-    virtual MStatus compute(const MPlug &plug, MDataBlock &data);
-    static void * creator();
+    AutoRollNode();
+    virtual ~AutoRollNode();
+    virtual MStatus compute(const MPlug& plug, MDataBlock& data);
+    static void* creator();
     static MStatus initialize();
 
+public:
     static MTypeId id;
-
-    static MObject inputTranslate;
-    static MObject inputRotate;
-    static MObject outputRotate;
+    static MObject inMatrix;
+    static MObject distance;
+    static MObject radius;
+    static MObject speed;
+    static MObject axis;
+    static MObject direction;
+    static MObject outMatrix;
+    static MObject outRoll;
+    static MObject outPitch;
+    static MObject outYaw;
 };
+
+#endif // AUTOROLLNODE_H
