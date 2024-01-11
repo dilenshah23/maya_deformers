443cd08a7a2c3e730f39899ad96f9350c090493f
Dilen Shah <dilenshah23@gmail.com>
2024-01-11 12:05:12 +1030

    Update WrinkleDeformer.h
    cpp update

--- wrinkleDeformer/cpp/source/WrinkleDeformer.h
+++ wrinkleDeformer/cpp/source/WrinkleDeformer.h
@@ -1,25 +1,39 @@
-#ifndef __WRINKLEDEFORMER_H__
-#define __WRINKLEDEFORMER_H__
+#ifndef WRINKLEDEFORMER_H
+#define WRINKLEDEFORMER_H
 
+// Include necessary headers from Maya
 #include <maya/MPxDeformerNode.h>
-#include <maya/MDataBlock.h>
-#include <maya/MItGeometry.h>
 #include <maya/MTypeId.h>
-#include <maya/MFnNumericAttribute.h>
+#include <maya/MItGeometry.h>
 
 class WrinkleDeformer : public MPxDeformerNode
 {
 public:
+    // Constructor and Destructor
     WrinkleDeformer();
     virtual ~WrinkleDeformer();
 
+    // Creator method for Maya to instantiate this deformer
     static void* creator();
-    static MStatus initialize();
-    virtual MStatus deform(MDataBlock& data, MItGeometry& itGeo, const MMatrix& localToWorldMatrix, unsigned int geomIndex);
 
+    // Method for initializing the attributes of the node
+    static MStatus initialize();
+
+    // The main deformation function that will be overridden from the parent class
+    virtual MStatus deform(MDataBlock& dataBlock, MItGeometry& iter, const MMatrix& mat, unsigned int multiIndex);
+
+    // Unique ID to identify this deformer node type
     static MTypeId id;
-    static MObject aAmount;
-    static MObject aDirection;
+
+    // Attributes of the deformer
+    static MObject intensityAttr; // Attribute to control the intensity of the wrinkle effect
+    static MObject paintMapAttr;  // Attribute to control the paint map
+
+private:
+    // Helper method to calculate compression for wrinkle effect
+    float calculateCompression(const MItGeometry& iter);
+
+    // Additional private members and methods can be declared here
 };
 
-#endif // __WRINKLEDEFORMER_H__
+#endif // WRINKLEDEFORMER_H
